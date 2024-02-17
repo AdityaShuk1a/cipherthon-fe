44d3751f6043bf332f142a48fa4578fa45f07706
AdityaShuk1a <142577439+AdityaShuk1a@users.noreply.github.com>
2024-02-18 01:44:14 +0530

    grid container

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ import { Nav } from "./components/Nav/Nav.jsx";
 import { Login } from "./components/Login/Login.jsx";
 import { SignUp } from "./components/Login/SignUp.jsx";
 import DoctorSignup from "./components/Register/DoctorSignup.jsx";
-
+import HomePage from "./components/HomePage/HomePage.jsx";
 const App = () => {
     return (
         <Box fontFamily={"unset"}>
-            <Nav></Nav>
+            <HomePage/>
         </Box>
     );
 };
