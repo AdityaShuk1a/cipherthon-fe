2254c6c6a2fdbb29d4073dcb7f40200a797e4e04
AdityaShuk1a <142577439+AdityaShuk1a@users.noreply.github.com>
2024-02-18 05:10:08 +0530

    fir humne mara ye wala

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,96 +6,99 @@ import {
     HStack,
     Text,
     Input,
+    Grid,
+    Button,
+    GridItem
 } from "@chakra-ui/react";
 import React from "react";
 
 import { HospitalSVG } from "../../resources/svg/Hospital";
+import { FaBold } from "react-icons/fa";
 const Login = () => {
     return (
         <>
-            <Box zIndex={-3}>
-                <Box
-                    p={15}
-                    ml={330}
-                    w={"fit-content"}
-                    h={"fit-content"}
-                    zIndex={-1}
-                    position={"absolute"}
-                >
-                    <Flex zIndex={100}>
-                        <Box fontSize={45} mt={370} ml={120} color={"navy"}>
+            
+                <Grid templateColumns={"repeat(2,1fr)"} >
+                    <GridItem bgColor={"whitesmoke"} borderTopLeftRadius={30}style={{borderRight:'2px solid white'}} borderBottomLeftRadius={30} fontSize={45} h={560} mt={220} ml={470} color={"navy"}>
+                        <Box mt={120} p={15}>
                             <Text fontWeight={60}>
                                 A Hospital
                                 <Text fontWeight={"bold"}>Bed Is A Parked Taxi</Text>
                                 <Text>With A Meter Runnnig</Text>
                             </Text>
                         </Box>
-                        <Box
-                            p={"33"}
-                            position={"absolute"}
-                            w={480}
-                            top={"220"}
-                            left={"650"}
-                            alignItems="left"
-                            textAlign={"left"}
-                            justifyContent="left"
-                            bgColor={"whitesmoke"}
-                            filter="opacity(50)"
-                        >
-                            <Flex>
-                                <Box
-                                    alignItems="left"
-                                    justifyContent={"left"}
-                                    display={"flex"}
-                                    flexDirection={"column"}
-                                    p={10}
-                                >
-                                    <HStack>
-                                        <HospitalSVG
-                                            style={{ transform: "scale(0.5)" }}
-                                            width="128"
-                                            height="128"
-                                        />
-                                        <Text ml={15} fontSize={"30"}>
-                                            JaMa
-                                        </Text>
-                                    </HStack>
-                                    <Text fontSize={20} mt={10} p={2}>
-                                        Welcome o Hospital JaMa
-                                    </Text>
-                                    <Box mt={"10"}>
-                                        <VStack>
-                                            <form>
-                                                <VStack>
-                                                    <Input
-                                                        w={320}
-                                                        type="email"
-                                                        placeholder="Email Address"
-                                                    ></Input>
+                    </GridItem>
+                    <GridItem
+                        p={18}
+                        borderBottomRightRadius={30}
+                        borderTopRightRadius={30}
+                        w={480}
+                        mt={"220"}
+                        left={"950"}
+                        alignItems="left"
+                        textAlign={"left"}
+                        justifyContent="left"
+                        bgColor={"whitesmoke"}
+                        filter="opacity(50)"
+                    >
+                        <Flex>
+                            <Box
+                                alignItems="left"
+                                justifyContent={"left"}
+                                display={"flex"}
+                                flexDirection={"column"}
+                                p={10}
+                            >
+                                <HStack>
+                                    <HospitalSVG
+                                        style={{ transform: "scale(0.5)" }}
+                                        width="128"
+                                        height="128"
+                                    />
+                                
 
-                                                    <Input
-                                                        type="password"
-                                                        placeholder="Password"
-                                                    ></Input>
-                                                </VStack>
-                                            </form>
-                                            <Box justifyContent={"left"} mt={"10"}>
-                                                <Text>
-                                                    Not a user Sign In ?{" "}
-                                                    <Link color={"blue"} href="#Register">
-                                                        Register
-                                                    </Link>
-                                                </Text>
-                                            </Box>
-                                        </VStack>
-                                    </Box>
+                                    
+                                        <HStack><Text  fontWeight={'Bold'} fontSize={"25"}>HealtH </Text><Box fontSize={"25"} bgColor={"navy"} p={2} borderRadius={5} color={"white"} fontWeight={'Bold'} mr={50} >HuB</Box></HStack>
+                                    
+                                
+                                </HStack>
+                                <Text  fontSize={20}  p={1}>
+                                    Welcome to Hospital HealtH HuB
+                                </Text>
+                                <Box mt={"10"}>
+                                    <VStack>
+                                        <form>
+                                            <VStack>
+                                                <Input
+                                                    w={320}
+                                                    type="email"
+                                                    placeholder="Email Address"
+                                                ></Input>
+
+                                                <Input
+                                                    type="password"
+                                                    placeholder="Password"
+                                                ></Input>
+                                                <Button mt={7}  bgColor={"navy"} type='button' color={"white"} >Submit</Button>
+                                            </VStack>
+                                        </form>
+                                        <Box justifyContent={"left"} mt={"10"}>
+                                            <Text>
+                                                Not a user Sign In ?{" "}
+                                                <Link color={"blue"} href="#Register">
+                                                    Register
+                                                </Link>
+                                            </Text>
+                                        </Box>
+                                    </VStack>
                                 </Box>
-                            </Flex>
-                        </Box>
-                    </Flex>
-                </Box>
-            </Box>
+                            </Box>
+                        </Flex>
+                    </GridItem>
+                </Grid>
+            
         </>
     );
 };
+
 export default Login;
