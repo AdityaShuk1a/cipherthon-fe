f5031cd92606acc24ae03df07edacc1ad3126215
Ravi-Singh-Chauhan <rvsng028@gmail.com>
2024-02-17 23:17:22 +0530

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,17 @@ import {
     space,
 } from "@chakra-ui/react";
 import React from "react";
-import { Nav } from "./components/Nav.jsx";
+import { Nav } from "./components/Nav/Nav.jsx";
 import { Login } from "./components/Login/Login.jsx";
 import { SignUp } from "./components/Login/SignUp.jsx";
-
 import DoctorSignup from "./components/Register/DoctorSignup.jsx";
 
 const App = () => {
     return (
         <Box fontFamily={"unset"}>
-            {/* <Login></Login> */}
-            <DoctorSignup />
+            <Nav></Nav>
         </Box>
     );
 };
-
 
 export default App;
