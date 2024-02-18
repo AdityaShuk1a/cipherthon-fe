7bf8eb196b9f2063c1658bb2cdc1437a427e0e54
Ravi-Singh-Chauhan <rvsng028@gmail.com>
2024-02-18 06:22:51 +0530

    navFixed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,50 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import { Box, Container, Flex,Image, Spacer, VStack,HStack,Button, background, flexbox,Text, space, MenuItem, MenuGroup } from '@chakra-ui/react'
-// 1. Import
-import { Icon } from '@chakra-ui/react'
+import { Box, MenuItem, MenuGroup } from "@chakra-ui/react";
 import { FiUser } from "react-icons/fi";
 import { FaPlus } from "react-icons/fa";
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-MenuDivider,
-} from '@chakra-ui/react'
+import { Menu, MenuButton, MenuList, MenuDivider } from "@chakra-ui/react";
 
-
-
-
-export const Nav=()=> {
-    const marginAll ={
-        margin:'20px 0 0 20px',
-        padding:'0',
-    }
-  return (
-    <Box display={'flex'} bgColor={'white'} justifyContent={'space-between'}   >
-        <Box mt={3} ml={4} p={2} bgColor={'white'}  borderRadius={50} >
-
-        <FaPlus style={{fontSize:'3vh'}} />
+export const Nav = () => {
+    return (
+        <Box display={"flex"} bgColor={"white"} justifyContent={"space-between"}>
+            <Box mt={3} ml={4} p={2} bgColor={"white"} borderRadius={50}>
+                <FaPlus style={{ fontSize: "3vh" }} />
+            </Box>
+            <Box mt={3} mr={4} p={2} bgColor={"white"} borderRadius={50}>
+                <Menu>
+                    <MenuButton colorScheme="blue">
+                        <FiUser style={{ fontSize: "3vh" }} />
+                    </MenuButton>
+                    <MenuList>
+                        <MenuGroup title="Profile">
+                            <MenuItem>My Account</MenuItem>
+                        </MenuGroup>
+                        <MenuDivider />
+                        <MenuGroup>
+                            <MenuItem>Log Out</MenuItem>
+                        </MenuGroup>
+                    </MenuList>
+                </Menu>
+            </Box>
         </Box>
-        <Box mt={3} mr={4} p={2} bgColor={'white'}  borderRadius={50} >
-            <Menu>
-  <MenuButton  colorScheme='blue'>
-    <FiUser style={{fontSize:'3vh'}}/>
-  </MenuButton>
-  <MenuList>
-    <MenuGroup title='Profile'>
-      <MenuItem>My Account</MenuItem>
-      
-    </MenuGroup>
-    <MenuDivider />
-    <MenuGroup>
-
-    <MenuItem>Log Out</MenuItem>
-    </MenuGroup>
-  </MenuList>
-</Menu>
-        
-        </Box>
-    </Box>
-  )
-}
+    );
+};
